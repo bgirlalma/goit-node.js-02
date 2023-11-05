9033edea7e103ac258cf1e9dc5109f66343891c8
Alina Akulina <124092247+bgirlalma@users.noreply.github.com>
2023-11-05 15:57:10 +0300

    models/contacts.js/done

--- models/contacts.js
+++ models/contacts.js
@@ -1,14 +1,93 @@
-// const fs = require('fs/promises')
+const fs = require('fs/promises');
+const path = require('path');
+const crypto = require('crypto');
+const contactsPath = path.join(__dirname, "models", "contacts.json");
 
-const listContacts = async () => {}
+async function readContacts(){
+  try {
+    const data = await fs.readFile(contactsPath, { encoding: 'utf-8'});
+    return JSON.parse(data)
+  } catch (error) {
+    console.error('Error reading contacts:', error);
+    return [];
+  }
+}
 
-const getContactById = async (contactId) => {}
+async function writeContacts(contacts){
+try {
+ await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2))
+} catch (error) {
+  console.error('Error writing contacts:', error);
+}
+}
 
-const removeContact = async (contactId) => {}
+const listContacts = async () => {
+  const contacts = await readContacts();
+  return contacts;
+}
 
-const addContact = async (body) => {}
+const getContactById = async (contactId) => {
+  const contacts = await readContacts();
 
-const updateContact = async (contactId, body) => {}
+  const contact = contacts.find((contact) => contact.id === contactId);
+  return contact;
+}
+
+const removeContact = async (contactId) => {
+  const contacts = await readContacts();
+  const index = contacts.findIndex((contact) => contact.id === contactId);
+  if(index === -1){
+    return null
+  }
+
+  contacts.split(index, 1)
+  await writeContacts(contacts)
+  return contacts;
+}
+
+const addContact = async (body) => {
+try {
+  const contacts = await readContacts();
+  const {name, email, phone} = body;
+  const newContacts = {
+    id: crypto.randomUUID(),
+    name,
+    email,
+    phone
+  };
+
+  contacts.push(newContacts);
+  return contacts;
+} catch (error) {
+  console.error(error);
+  throw error;
+}
+}
+
+const updateContact = async (contactId, body) => {
+try {
+  const contacts = await readContacts();
+  const index = contacts.findIndex((contact) => contact.id === contactId)
+  const {id, name, email, phone} = body
+
+  const update = {
+   id,
+   name,
+   email,
+   phone
+  }
+
+  if(index !== -1){
+    throw new Error('Контакт не найден');  // выбрасываем сообщение если контакт не найден
+  };
+
+  contacts[index] = update;  // Обновляем контакт в массиве
+  return update;  // Возвращаем обновленный контакт
+} catch (error) {
+  console.error(error);
+  throw error;
+}
+}
 
 module.exports = {
   listContacts,
